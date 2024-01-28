558b9ab0a7f3e65b4182611006e3af4e6d43bd4d
Nikhil Reddy <nikhilreddy3111@gmail.com>
2024-01-27 22:33:27 -0500

    sg code

--- main.tf
+++ main.tf
@@ -2,6 +2,13 @@ resource "aws_security_group" "demo-sg" {
   name   = var.sg_name
   vpc_id = var.vpc__id
   ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = var.sg_ingress_protocol
+    cidr_blocks = var.sg_egress_cidr
+  }
+
+  ingress {
     from_port   = 22
     to_port     = 22
     protocol    = var.sg_ingress_protocol
